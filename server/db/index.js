57362172728575778f46717543c10b1a56e0d86b
Ivan Mironchik <me.roaiven@gmail.com>
2016-03-22 14:34:50 +0300

    Implemented "cards" table

--- server/db/index.js
+++ server/db/index.js
@@ -7,8 +7,10 @@ const db = pgp(config.db);
 
 const boards = fs.readFileSync('server/db/tables/boards.sql', 'utf8');
 const lists = fs.readFileSync('server/db/tables/lists.sql', 'utf8');
+const cards = fs.readFileSync('server/db/tables/cards.sql', 'utf8');
 
 db.query(boards);
 db.query(lists);
+db.query(cards);
 
 module.exports = db;
