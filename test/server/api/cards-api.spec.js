d9cafafe5c2f6af1dda6b42ebb637ef6527e9d54
Ivan Mironchik <me.roaiven@gmail.com>
2016-03-26 00:36:10 +0300

    Replaced multiple promise call on helper functions in "cards api" tests

--- test/server/api/cards-api.spec.js
+++ test/server/api/cards-api.spec.js
@@ -1,27 +1,23 @@
 import { assert } from 'chai';
 import cardsApi from 'server/api/cards-api';
-import fs from 'fs';
+import { sql } from 'server/helpers';
 import db from 'server/db';
-
-const createCardsSql = fs.readFileSync('server/db/tables/cards.sql', 'utf8');
+import { createCards } from './helpers';
 
 describe('cards api', () => {
     beforeEach(() => {
         return db.query('DROP TABLE IF EXISTS cards')
-        .then(() => db.query(createCardsSql));
+        .then(() => db.query(sql('cards.sql')));
     });
 
     describe('getSome', () => {
         it('should return cards by given ids', () => {
-            return cardsApi.create({text: 'test card 1'})
-            .then(() => cardsApi.create({text: 'test card 2'}))
-            .then(() => cardsApi.create({text: 'test card 3'}))
-            .then(() => cardsApi.create({text: 'test card 4'}))
+            return createCards()
             .then(() => cardsApi.getSome([2, 3]))
             .then(cards => {
                 const expected = [
-                    { id: 2, text: 'test card 2' },
-                    { id: 3, text: 'test card 3' }
+                    { id: 2, text: 'cards entry 2' },
+                    { id: 3, text: 'cards entry 3' }
                 ];
                 assert.deepEqual(cards, expected);
             });
