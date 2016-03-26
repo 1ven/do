aa49ea7f3d48ddc5349de95897e3cdf05b56aa2c
Ivan Mironchik <me.roaiven@gmail.com>
2016-03-26 18:45:58 +0300

    Implemented test on /lists/create route

--- server/routes/lists-routes.js
+++ server/routes/lists-routes.js
@@ -3,9 +3,18 @@
 const boardsApi = require('../api/boards-api');
 const listsApi = require('../api/lists-api');
 
-module.exports = (post)=> {
+module.exports = (post) => {
     post('/lists/create', body => {
         return listsApi.create({ title: body.title })
-        .then(result => boardsApi.addList(body.boardId, result.id));
+        .then(result => {
+            const listId = result.id;
+            return boardsApi.addList(body.boardId, listId)
+            .then(() => { return { listId }; });
+        });
     });
+
+    // post('/lists/remove', body => {
+    //     return listsApi.remove(body.listId)
+    //     .then(() => boardsApi.removeList(body.boardId, body.listId));
+    // });
 };
--- test/server/routes/lists-routes.spec.js
+++ test/server/routes/lists-routes.spec.js
@@ -3,25 +3,37 @@ import request from 'supertest';
 import boardsApi from 'server/api/boards-api';
 import listsApi from 'server/api/lists-api';
 import app from 'server/.';
-import { handleEndRequest, recreateTables } from '../helpers';
+import { createBoards, handleEndRequest, recreateTables } from '../helpers';
 
 describe('lists routes', () => {
     beforeEach(recreateTables);
 
-    it('/lists/create should respond with json', (done) => {
-        boardsApi.create({
-            title: 'test board'
-        })
-        .then(result => {
-            return request(app)
+    it('/lists/create should create list and place it on board', (done) => {
+        const boardId = 6;
+
+        createBoards()
+        .then(() => {
+            request(app)
             .post('/lists/create')
+            .set('Accept', 'application/json')
             .send({
-                title: "test",
-                boardId: 1
+                title: 'test',
+                boardId
             })
             .expect('Content-Type', /json/)
             .expect(200)
-            .end(handleEndRequest.bind(null, { success: true }, done));
+            .end((requestErr, res) => {
+                if (requestErr) { done(requestErr); }
+
+                const listId = res.body.data.listId;
+
+                listsApi.getById(listId)
+                .then(() => boardsApi.getById(boardId))
+                .then(board => {
+                    assert.include(board.lists, listId);
+                })
+                .then(done, err => done(err));
+            });
         });
     });
 
