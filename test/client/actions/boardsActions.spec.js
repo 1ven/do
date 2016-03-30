86f1e40cd27362a73a762b01946f783d58be9773
Ivan Mironchik <me.roaiven@gmail.com>
2016-03-31 00:27:50 +0300

    Implemented "BOARDS_GET_ERROR" action test

--- test/client/actions/boardsActions.spec.js
+++ test/client/actions/boardsActions.spec.js
@@ -19,18 +19,39 @@ describe('boardsActions', () => {
                 { id: 1, title: 'board 1', lists: null },
                 { id: 2, title: 'board 2', lists: null }
             ];
-            nock('http://localhost').post('/boards/get-all')
-                .reply(200, { success: true, data: boards });
-
-            const store = mockStore({ boards: [] });
             const expectedActions = [
                 { type: types.BOARDS_GET_START },
                 { type: types.BOARDS_GET_SUCCESS, payload: boards }
             ];
+            const store = mockStore({ boards: [] });
+
+            nock('http://localhost').post('/boards/get-all')
+                .reply(200, { success: true, data: boards });
 
             return store.dispatch(actions.getBoards())
                 .then(() => {
                     assert.deepEqual(store.getActions(), expectedActions);
+                });
+        });
+
+        it('should create BOARDS_GET_ERROR action when getting boards has been done with error', () => {
+            const errorMessage = 'Test error message';
+            const expectedActions = [
+                { type: types.BOARDS_GET_START },
+                { type: types.BOARDS_GET_ERROR, payload: new Error(errorMessage), error: true }
+            ];
+            const store = mockStore({ boards: [] });
+
+            nock('http://localhost').post('/boards/get-all')
+                .reply(200, { success: false, error: errorMessage });
+
+            return store.dispatch(actions.getBoards())
+                .then(() => {
+                    const actions = store.getActions();
+                    const err = actions[1].payload;
+
+                    assert.deepEqual(actions, expectedActions);
+                    assert.equal(err.message, errorMessage);
                 });
         });
     });
