9ba1446dd651f4258ceb925414ac0b922049170b
Ivan Mironchik <me.roaiven@gmail.com>
2016-03-30 22:55:43 +0300

    Added specific hostname for test environment on fetch requests

--- client/utils/fetchWrapper.js
+++ client/utils/fetchWrapper.js
@@ -1,7 +1,9 @@
 import fetch from 'isomorphic-fetch';
 
 export default function (url, options) {
-    return fetch(url, options)
+    const hostname = process.env.NODE_ENV === 'test' ? 'http://localhost' : '';
+
+    return fetch(hostname + url, options)
         .then(result => result.json())
         .then(json => {
             if (!json.success) {
