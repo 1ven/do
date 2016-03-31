3e418fe7103c551fe4392b483fa4d6d7e7cc1eae
Ivan Mironchik <me.roaiven@gmail.com>
2016-03-31 12:39:06 +0300

    Implemented tests for "boardsReducer"

--- test/client/reducers/boardsReducer.spec.js
+++ test/client/reducers/boardsReducer.spec.js
@@ -4,56 +4,51 @@ import * as types from 'client/constants/actionTypes';
 
 describe('boards reducer', () => {
     it('should return initial state', () => {
-        const state = boardsReducer(undefined, {});
+        const nextState = boardsReducer(undefined, {});
         const expectedState = {
             loading: false,
             items: []
         };
-        assert.deepEqual(state, expectedState);
+        assert.deepEqual(nextState, expectedState);
     });
 
-    // it('should handle BOARDS:CREATE', () => {
-    //     const createAction = {
-    //         type: BOARDS.CREATE,
-    //         payload: {
-    //             title: 'test title',
-    //             id: 7
-    //         }
-    //     };
-    //     const state = boardsReducer([], createAction);
-    //     const expectedState = [
-    //         {
-    //             title: 'test title',
-    //             id: 7
-    //         }
-    //     ];
-    //     assert.deepEqual(state, expectedState);
-    // });
+    it('should handle BOARDS_GET_REQUEST', () => {
+        const action = {
+            type: types.BOARDS_GET_REQUEST
+        };
+        const state = { items: [], loading: false };
+        const expectedState = { items: [], loading: true };
+        const nextState = boardsReducer(state, action);
 
-    // it('should handle BOARDS:REMOVE', () => {
-    //     const removeAction = {
-    //         type: BOARDS.REMOVE,
-    //         payload: {
-    //             id: 2
-    //         }
-    //     };
-    //     const previousState = [
-    //         {
-    //             title: 'test title 1',
-    //             id: 1
-    //         },
-    //         {
-    //             title: 'test title 2',
-    //             id: 2
-    //         }
-    //     ];
-    //     const state = boardsReducer(previousState, removeAction);
-    //     const expectedState = [
-    //         {
-    //             title: 'test title 1',
-    //             id: 1
-    //         }
-    //     ];
-    //     assert.deepEqual(state, expectedState);
-    // });
+        assert.deepEqual(nextState, expectedState);
+    });
+
+    it('should handle BOARDS_GET_SUCCESS', () => {
+        const boards = [
+            { id: 1, title: 'board 1' },
+            { id: 2, title: 'board 2' }
+        ];
+        const action = {
+            type: types.BOARDS_GET_SUCCESS,
+            payload: boards
+        };
+        const state = { items: [], loading: true };
+        const expectedState = { items: boards, loading: false };
+        const nextState = boardsReducer(state, action);
+
+        assert.deepEqual(nextState, expectedState);
+    });
+
+    it('should handle BOARDS_GET_ERROR', () => {
+        const action = {
+            type: types.BOARDS_GET_ERROR,
+            payload: new Error('Test error'),
+            error: true
+        };
+        const state = { items: [], loading: true };
+        const expectedState = { items: [], loading: false };
+        const nextState = boardsReducer(state, action);
+
+        assert.deepEqual(nextState, expectedState);
+    });
 });
