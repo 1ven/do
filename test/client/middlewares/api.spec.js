5e3012bee53983245dcd4ddf73d8c262dfdb04a4
Ivan Mironchik <me.roaiven@gmail.com>
2016-04-05 22:30:28 +0300

    Implemented api middleware test and removed boards action tests

--- test/client/middlewares/api.spec.js
+++ test/client/middlewares/api.spec.js
@@ -1,39 +1,85 @@
-// import { Schema } from 'normalizr';
-// import nock from 'nock';
-// import configureMockStore from 'redux-mock-store';
-// import apiMiddleware, { CALL_API } from 'client/middlewares/api';
+import { Schema, arrayOf } from 'normalizr';
+import { assert } from 'chai';
+import assign from 'lodash/assign';
+import nock from 'nock';
+import configureMockStore from 'redux-mock-store';
+import apiMiddleware, { CALL_API } from 'client/middlewares/api';
 
-// const middlewares = [ apiMiddleware ];
-// const mockStore = configureMockStore(middlewares);
+const middlewares = [ apiMiddleware ];
+const mockStore = configureMockStore(middlewares);
 
-// const types = {
-//     REQUEST: 'REQUEST',
-//     SUCCESS: 'SUCCESS',
-//     ERROR: 'ERROR'
-// };
-// const apiAction = {
-//     [CALL_API]: {
-//         types: [
-//             types.REQUEST,
-//             types.SUCCESS,
-//             types.ERROR
-//         ],
-//         endpoint: '/test',
-//     }
-// };
+function testApi(action, expectedActions, cb) {
+    const store = mockStore();
+    const items = [ { id: 1, title: 'test' } ];
+    const assertion = () => cb ? cb.bind(null, store.getActions) : assert.deepEqual(store.getActions(), expectedActions);
 
-// describe('apiMiddleware', () => {
-//     beforeEach(() => {
-//         nock.cleanAll();
-//     });
+    nock('http://localhost')
+        .get('/test')
+        .reply(200, { success: true, data: items });
 
-//     it('should call request to api', (done) => {
-//         const store = mockStore();
+    return store.dispatch(action).then(assertion);
+};
 
-//         nock('http://localhost')
-//             .get('/test')
-//             .reply(200);
+describe('apiMiddleware', () => {
+    beforeEach(() => {
+        nock.cleanAll();
+    });
 
-//         store.dispatch(apiAction);
-//     });
-// });
+    it('should call request to api, dispatch REQUEST and SUCCESS action', () => {
+        const action = {
+            [CALL_API]: {
+                types: [ 'REQUEST', 'SUCCESS', 'ERROR' ],
+                endpoint: '/test',
+            }
+        };
+
+        const expectedActions = [
+            { type: 'REQUEST' },
+            { type: 'SUCCESS', payload: [ { id: 1, title: 'test' } ] }
+        ];
+
+        return testApi(action, expectedActions);
+    });
+
+    it('should normalize JSON response, when schema is provided', () => {
+        const schema = new Schema('test');
+        const action = {
+            [CALL_API]: {
+                types: [ 'REQUEST', 'SUCCESS', 'ERROR' ],
+                endpoint: '/test',
+                schema: arrayOf(schema)
+            }
+        };
+
+        const normalized = {
+            result: [ 1 ],
+            entities: {
+                test: {
+                    1: { id: 1, title: 'test' }
+                }
+            }
+        };
+
+        const expectedActions = [
+            { type: 'REQUEST' },
+            { type: 'SUCCESS', payload: normalized }
+        ];
+
+        return testApi(action, expectedActions);
+    });
+
+    it('should dispatch ERROR action when request returned an error', () => {
+        const action = {
+            [CALL_API]: {
+                types: [ 'REQUEST', 'SUCCESS', 'ERROR' ],
+                endpoint: '/unknown',
+            }
+        };
+
+        return testApi(action, null, (getActions) => {
+            const actions = getActions();
+            assert.equal(actions[0], { type: 'REQUEST' });
+            assert.equal(actions[0], { type: 'ERROR' });
+        });
+    });
+});
