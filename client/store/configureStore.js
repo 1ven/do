ab1a683ae7e86e948424505e7e4e27029bdbe93f
Ivan Mironchik <me.roaiven@gmail.com>
2016-04-05 14:53:07 +0300

    Applied api middleware

--- client/store/configureStore.js
+++ client/store/configureStore.js
@@ -1,12 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers';
-import { thunk, logger } from '../middlewares';
+import { thunk, logger, api } from '../middlewares';
 
 export default function configureStore(initialState) {
     return createStore(
         rootReducer,
         initialState,
         applyMiddleware(
+            api,
             thunk,
             logger
         )
