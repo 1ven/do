0894e938d8af00492d7acd4e2f64b192ae2811df
Ivan Mironchik <me.roaiven@gmail.com>
2016-04-24 13:57:04 +0300

    Implemented registration function in user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const _ = require('lodash');
+const crypto = require('crypto');
 const Base = require('./Base');
 
 const User = _.assign({}, Base, {
@@ -8,7 +9,25 @@ const User = _.assign({}, Base, {
     immutableFields: ['id', 'username', 'hash', 'salt'],
     hiddenFields: ['hash', 'salt', 'email'],
 
-    add(props) {},
+    register(props) {
+        if (!props || !props.password) {
+            throw new Error('Password is not provided');
+        }
+
+        const salt = Math.random() + '';
+        const hash = this.encryptPassword(props.password, salt);
+
+        delete props.password;
+
+        return this.create(_.assign({}, props, {
+            hash,
+            salt
+        }));
+    },
+
+    encryptPassword(password, salt) {
+        return crypto.createHash('md5').update(password + salt).digest('hex');
+    }
 });
 
 module.exports = User;
--- test/server/models/User.spec.js
+++ test/server/models/User.spec.js
@@ -1,11 +1,38 @@
-// import chai, { assert } from 'chai';
-// import chaiaspromised from 'chai-as-promised';
-// import { recreateTables } from '../helpers';
-// import db from 'server/db';
-// import User from 'server/models/User';
+import chai, { assert } from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import { recreateTables } from '../helpers';
+import db from 'server/db';
+import User from 'server/models/User';
 
-// chai.use(chaiAsPromised);
+chai.use(chaiAsPromised);
 
-// describe('User model', () => {
-//     beforeEach(recreateTables);
-// });
+describe('User model', () => {
+    beforeEach(recreateTables);
+
+    describe('register', () => {
+        it('should create user', () => {
+            const props = {
+                username: 'test',
+                email: 'test@mail.com',
+                password: 123456
+            };
+            return User.register(props)
+                .then(() => {
+                    return db.one('SELECT * FROM users WHERE id = 1');
+                })
+                .then(user => {
+                    assert.property(user, 'hash');
+                    assert.property(user, 'salt');
+
+                    delete user.hash;
+                    delete user.salt;
+
+                    assert.deepEqual(user, {
+                        id: 1,
+                        username: props.username,
+                        email: props.email
+                    });
+                });
+        });
+    });
+});
