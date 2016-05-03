825f5ee98fd8a3e756b86621e4fee5c0b8511aae
Ivan Mironchik <me.roaiven@gmail.com>
2016-05-03 14:36:04 +0300

    Implemented `db.tx()` in tables creating

--- server/db/index.js
+++ server/db/index.js
@@ -6,13 +6,18 @@ const pgp = require('pg-promise')({
 });
 const config = require('../config');
 const sql = require('../utils/sql');
+const fs = require('fs');
+const path = require('path');
 
 const db = pgp(config.db);
+const sessionSql = fs.readFileSync(path.resolve(__dirname, '../../node_modules/connect-pg-simple/table.sql'), 'utf8');
 
-db.none(sql('cards.sql'))
-    .then(() => db.none(sql('lists.sql')))
-    .then(() => db.none(sql('boards.sql')))
-    .then(() => db.none(sql('users.sql')))
-    .catch(err => { throw err; });
+// TODO: Create session table
+db.tx(function() {
+    return this.none(sql('cards.sql'))
+        .then(() => this.none(sql('lists.sql')))
+        .then(() => this.none(sql('boards.sql')))
+        .then(() => this.none(sql('users.sql')));
+}).catch(err => { throw err; });
 
 module.exports = db;
