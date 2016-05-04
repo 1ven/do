487c988dabff672c6e3d324100b6b8824f0d5ab7
Ivan Mironchik <me.roaiven@gmail.com>
2016-05-04 14:19:58 +0300

    Protected unsecure routes

--- server/routes.js
+++ server/routes.js
@@ -24,24 +24,24 @@ module.exports = function (app) {
         res.redirect('/sign-in');
     });
 
-    app.post('/auth/local', passport.authenticate('local', {
+    app.post('/auth/local', ensureLoggedOut, passport.authenticate('local', {
         failureRedirect: '/sign-in',
         successRedirect: '/'
     }));
 
-    app.get('/api/boards', handleRoute(BoardController, 'get'));
-    app.get('/api/boards/:id', handleRoute(BoardController, 'getOne'));
-    app.post('/api/boards', handleRoute(BoardController, 'create'));
-    app.post('/api/boards/:id/lists', handleRoute(BoardController, 'createList'));
-    app.delete('/api/boards/:id', handleRoute(BoardController, 'remove'));
-    app.put('/api/boards/:id', handleRoute(BoardController, 'update'));
+    app.get('/api/boards', ensureLoggedIn, handleRoute(BoardController, 'get'));
+    app.get('/api/boards/:id', ensureLoggedIn, handleRoute(BoardController, 'getOne'));
+    app.post('/api/boards', ensureLoggedIn, handleRoute(BoardController, 'create'));
+    app.post('/api/boards/:id/lists', ensureLoggedIn, handleRoute(BoardController, 'createList'));
+    app.delete('/api/boards/:id', ensureLoggedIn, handleRoute(BoardController, 'remove'));
+    app.put('/api/boards/:id', ensureLoggedIn, handleRoute(BoardController, 'update'));
 
-    app.post('/api/lists/:id/cards', handleRoute(ListController, 'createCard'));
-    app.delete('/api/lists/:id', handleRoute(ListController, 'remove'));
-    app.put('/api/lists/:id', handleRoute(ListController, 'update'));
+    app.post('/api/lists/:id/cards', ensureLoggedIn, handleRoute(ListController, 'createCard'));
+    app.delete('/api/lists/:id', ensureLoggedIn, handleRoute(ListController, 'remove'));
+    app.put('/api/lists/:id', ensureLoggedIn, handleRoute(ListController, 'update'));
 
-    app.delete('/api/cards/:id', handleRoute(CardController, 'remove'));
-    app.put('/api/cards/:id', handleRoute(CardController, 'update'));
+    app.delete('/api/cards/:id', ensureLoggedIn, handleRoute(CardController, 'remove'));
+    app.put('/api/cards/:id', ensureLoggedIn, handleRoute(CardController, 'update'));
 
     app.get('*', ensureLoggedIn, (req, res) => {
         res.render('index', {
