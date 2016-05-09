bd458da435cc0afd8a9845097b3cf88526a4ad54
Ivan Mironchik <me.roaiven@gmail.com>
2016-05-09 14:41:19 +0300

    Implemented <Header />

--- client/components/App.js
+++ client/components/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import NoticeContainer from '../containers/NoticeContainer';
+import Header from './Header';
 
 export default ({ children }) => (
     <div>
-        {children}
+        <Header
+            user={{
+                username: 'roaiven',
+                role: 'web developer'
+            }}
+        />
+        <div className="b-route-handler">
+            {children}
+        </div>
         <NoticeContainer />
     </div>
 );
---
+++ client/components/Header.js
@@ -0,0 +1,30 @@
+import React, { PropTypes } from 'react';
+import UserBox from './UserBox';
+
+const Header = function ({ user }) {
+    return (
+        <header className="b-header">
+            <div className="b-container">
+                <div className="b-header__wrap">
+                    <div className="b-header__left">
+                    </div>
+                    <div className="b-header__right">
+                        <div className="b-header__item">
+                            <UserBox {...user} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+Header.propTypes = {
+    user: PropTypes.shape({
+        avatar: PropTypes.string,
+        username: PropTypes.string.isRequired,
+        role: PropTypes.string.isRequired
+    }).isRequired
+};
+
+export default Header;
