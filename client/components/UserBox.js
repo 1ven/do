74dcd400a77e4ad0fe06fe38fa974886cc8e5b46
Ivan Mironchik <me.roaiven@gmail.com>
2016-05-09 22:01:44 +0300

    Added toggling menu using <Toggle />

--- client/components/UserBox.js
+++ client/components/UserBox.js
@@ -1,70 +1,48 @@
 import React, { PropTypes, Component } from 'react';
 import MenuList from './MenuList';
+import Toggle from './Toggle';
 
-class UserBox extends Component {
-    constructor(props) {
-        super(props);
-
-        this.handleClick = this.handleClick.bind(this);
-
-        this.state = {
-            isMenuVisible: false
-        };
-    }
-
-    handleClick(e) {
-        e.preventDefault();
-
-        const { isMenuVisible } = this.state;
-
-        this.setState({
-            isMenuVisible: !isMenuVisible
-        });
-    }
-
-    render() {
-        const { username, role, avatar } = this.props;
-        const { isMenuVisible } = this.state;
-
-        let menu;
-
-        if (isMenuVisible) {
-            menu = (
-                <div className="b-user-box__menu">
-                    <MenuList
-                        items={[
-                            { title: 'Sign out', href: '/sign-out' }
-                        ]}
-                    />
-                </div>
-            );
-        }
-
-        return (
-            <div className="b-user-box">
-                <div
-                    className="b-user-box__wrap"
-                    onClick={this.handleClick}
-                >
-                    <div className="b-user-box__left">
-                        <span className="b-user-box__username">
-                            @{username}
-                        </span>
-                        <span className="b-user-box__role">
-                            {role}
-                        </span>
-                    </div>
-                    <div className="b-user-box__right">
-                        <img
-                            className="b-user-box__avatar"
-                            src={avatar}
-                        />
-                    </div>
-                </div>
-                {menu}
+const UserBox = function ({ username, role, avatar }) {
+    const wrap = (
+        <div
+            className="b-user-box__wrap"
+            onClick={this.handleClick}
+        >
+            <div className="b-user-box__left">
+                <span className="b-user-box__username">
+                    @{username}
+                </span>
+                <span className="b-user-box__role">
+                    {role}
+                </span>
             </div>
-        );
-    }
+            <div className="b-user-box__right">
+                <img
+                    className="b-user-box__avatar"
+                    src={avatar}
+                />
+            </div>
+        </div>
+    );
+
+    const menu = (
+        <div className="b-user-box__menu">
+            <MenuList
+                items={[
+                    { title: 'Sign out', href: '/sign-out' }
+                ]}
+            />
+        </div>
+    );
+
+    return (
+        <div className="b-user-box">
+            <Toggle
+                link={wrap}
+                content={menu}
+            />
+        </div>
+    );
 };
 
 UserBox.defaultProps = {
