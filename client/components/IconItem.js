6af519da6a6f26b5459e53f882cd1a893c3fed1c
Ivan Mironchik <me.roaiven@gmail.com>
2016-05-09 21:31:45 +0300

    Implemented <IconItem />

---
+++ client/components/IconItem.js
@@ -0,0 +1,33 @@
+import React, { PropTypes } from 'react';
+import Icon from './Icon';
+
+const IconItem = function ({
+    children,
+    iconName,
+    iconWidth
+}) {
+    return (
+        <div className="b-icon-item">
+            <div
+                className="b-icon-item__icon"
+                style={{ width: iconWidth }}
+            >
+                <Icon name={iconName} />
+            </div>
+            <div className="b-icon-item__text">
+                {children}
+            </div>
+        </div>
+    );
+};
+
+IconItem.defaultProps = {
+    iconWidth: 14
+};
+
+IconItem.propTypes = {
+    iconName: PropTypes.string.isRequired,
+    iconWidth: PropTypes.string,
+};
+
+export default IconItem;
