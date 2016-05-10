c3cbc174fbdc3ec2f6fe33097efdebefe2c190b3
Ivan Mironchik <me.roaiven@gmail.com>
2016-05-10 09:28:42 +0300

    Added <Btn /> to <BottomBox />

--- client/components/BoardsList.js
+++ client/components/BoardsList.js
@@ -2,41 +2,50 @@ import React, { PropTypes } from 'react';
 import BoardTile from './BoardTile';
 import InputForm from './InputForm';
 import BottomBox from './BottomBox';
+import Btn from './Btn';
 
 const BoardsList = ({
     boards,
     onBoardCreatorSubmit,
     onBoardTileRemoveClick
-}) => (
-    <div className="c-boards-list">
-        <div className="b-container">
-            <div className="c-boards-list__items">
-                {boards.map((board, i) =>
-                    <div
-                        className="c-boards-list__item"
-                        key={i}
-                    >
-                        <BoardTile
-                            {...board}
-                            onRemoveClick={onBoardTileRemoveClick}
-                        />
-                    </div>
-                )}
-                <div className="c-boards-list__item" >
-                    <div className="b-tile">
-                        <InputForm
-                            placeholder="Enter board name"
-                            onSubmit={onBoardCreatorSubmit}
-                        />
+}) => {
+    const addBoardBtn = (
+        <Btn
+            text="Add new board"
+        />
+    );
+
+    return (
+        <div className="c-boards-list">
+            <div className="b-container">
+                <div className="c-boards-list__items">
+                    {boards.map((board, i) =>
+                        <div
+                            className="c-boards-list__item"
+                            key={i}
+                        >
+                            <BoardTile
+                                {...board}
+                                onRemoveClick={onBoardTileRemoveClick}
+                            />
+                        </div>
+                    )}
+                    <div className="c-boards-list__item" >
+                        <div className="b-tile">
+                            <InputForm
+                                placeholder="Enter board name"
+                                onSubmit={onBoardCreatorSubmit}
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
+            <BottomBox
+                button={addBoardBtn}
+            />
         </div>
-        <BottomBox
-            button={<div>1</div>}
-        />
-    </div>
-);
+    );
+};
 
 BoardsList.propTypes = {
     boards: PropTypes.arrayOf(PropTypes.shape({
