b552b20562eaf3832c76fb3072efd2d521e0244e
Ivan Mironchik <me.roaiven@gmail.com>
2016-05-10 22:44:00 +0300

    Implemented <ToggleMenu />

---
+++ client/components/ToggleMenu.js
@@ -0,0 +1,31 @@
+import React, { PropTypes } from 'react';
+import Icon from './Icon';
+import Toggle from './Toggle';
+
+function ToggleMenu({ menu }) {
+    const link = (
+        <div className="b-toggle-menu__link">
+            <Icon name="cog" />
+        </div>
+    );
+    const content = (
+        <div className="b-toggle-menu__content">
+            {menu}
+        </div>
+    );
+
+    return (
+        <div className="b-toggle-menu">
+            <Toggle
+                link={link}
+                content={content}
+            />
+        </div>
+    );
+};
+
+ToggleMenu.propTypes = {
+    menu: PropTypes.node.isRequired
+};
+
+export default ToggleMenu;
