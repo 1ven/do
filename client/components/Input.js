57227c11037d3664f29e309116f2d7e4f73b2a81
Ivan Mironchik <me.roaiven@gmail.com>
2016-05-11 17:05:11 +0300

    Added `focus` setting, added `onChange` event handler in <Input />

--- client/components/Input.js
+++ client/components/Input.js
@@ -1,15 +1,48 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
+import ReactDOM from 'react-dom';
 
-function Input(props) {
-    return (
-        <div className="b-input">
-            <input
-                className="b-input__node"
-                type="text"
-                {...props}
-            />
-        </div>
-    );
-};
+class Input extends Component {
+    constructor(props) {
+        super(props);
+
+        this.handleChange = this.handleChange.bind(this);
+
+        this.state = {
+            value: props.value || ''
+        };
+    }
+
+    componentDidMount() {
+        const { focus } = this.props;
+
+        focus && ReactDOM.findDOMNode(this.refs.input).focus();
+    }
+
+    handleChange(e) {
+        const value = e.target.value;
+        const { onChange } = this.props;
+
+        this.setState({ value });
+
+        onChange && onChange(e);
+    }
+
+    render() {
+        const { value } = this.state;
+
+        return (
+            <div className="b-input">
+                <input
+                    {...this.props}
+                    className="b-input__node"
+                    type="text"
+                    ref="input"
+                    value={value}
+                    onChange={this.handleChange}
+                />
+            </div>
+        );
+    }
+}
 
 export default Input;
