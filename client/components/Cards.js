0522b5cff9d7dce6fb9d35200868e0cc9e711348
Ivan Mironchik <me.roaiven@gmail.com>
2016-05-11 12:39:43 +0300

    Added appearing <InputForm /> by clicking at <Btn />

--- client/components/Cards.js
+++ client/components/Cards.js
@@ -1,45 +1,80 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import InputForm from './InputForm';
 import Card from './Card';
 import Btn from './Btn';
 
-const Cards = ({
-    cards = [],
-    onCardCreate,
-    onCardRemoveClick,
-    onCardInputFormSubmit,
-    onCardTextClick
-}) => {
-    return (
-        <div className="c-cards">
-            {cards.map((card, i) =>
-                <div
-                    key={i}
-                    className="c-cards__item"
-                >
-                    <Card
-                        {...card}
-                        onRemoveClick={onCardRemoveClick}
-                        onInputFormSubmit={onCardInputFormSubmit}
-                        onTextClick={onCardTextClick}
-                    />
-                </div>
-            )}
-            <div className="c-cards__item">
-                {false ? (
-                    <InputForm
-                        onSubmit={onCardCreate}
-                        placeholder="Enter card name"
-                    />
-                ) : (
-                    <Btn
-                        text="Add new card"
-                        modifiers={['full_width', 'sm']}
-                    />
+class Cards extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isInputFormVisible: false
+        };
+
+        this.handleInputFormCrossClick = this.handleInputFormCrossClick.bind(this);
+        this.handleAddCardBtnClick = this.handleAddCardBtnClick.bind(this);
+    }
+
+    handleInputFormCrossClick() {
+        this.setState({
+            isInputFormVisible: false
+        });
+    }
+
+    handleAddCardBtnClick() {
+        this.setState({
+            isInputFormVisible: true
+        });
+    }
+
+    render() {
+        const {
+            cards,
+            onCardCreate,
+            onCardRemoveClick,
+            onCardInputFormSubmit,
+            onCardTextClick
+        } = this.props;
+        const { isInputFormVisible } = this.state;
+
+        return (
+            <div className="b-cards">
+                {cards.map((card, i) =>
+                    <div
+                        key={i}
+                        className="b-cards__item"
+                    >
+                        <Card
+                            {...card}
+                            onRemoveClick={onCardRemoveClick}
+                            onInputFormSubmit={onCardInputFormSubmit}
+                            onTextClick={onCardTextClick}
+                        />
+                    </div>
                 )}
+                <div className="b-cards__item">
+                    {isInputFormVisible ? (
+                        <InputForm
+                            onSubmit={onCardCreate}
+                            placeholder="Enter card text"
+                            onCrossClick={this.handleInputFormCrossClick}
+                            autoFocus={true}
+                        />
+                    ) : (
+                        <Btn
+                            text="Add new card"
+                            modifiers={['full_width', 'sm']}
+                            onClick={this.handleAddCardBtnClick}
+                        />
+                    )}
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
+}
+
+Cards.defaultProps = {
+    cards: []
 };
 
 Cards.propTypes = {
