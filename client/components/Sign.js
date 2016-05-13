f154ea2374b8467d763a363f245d0faecfdf19da
Ivan Mironchik <me.roaiven@gmail.com>
2016-05-13 10:48:20 +0300

    Implemented `b-sign` stylesheet

--- client/components/Sign.js
+++ client/components/Sign.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 export default ({ children }) => (
     <div className="b-sign">
         <div className="b-sign__middle">
+            <div className="b-sign__nav">
+                <Link
+                    className="b-sign__nav-link"
+                    activeClassName="b-sign__nav-link_active"
+                    to="/sign-in"
+                >
+                    Sign in
+                </Link>
+                <Link
+                    className="b-sign__nav-link"
+                    activeClassName="b-sign__nav-link_active"
+                    to="/sign-up"
+                >
+                    Sign up
+                </Link>
+            </div>
             {children}
         </div>
     </div>
