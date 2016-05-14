b1b4e2e33972e8c6ad341b7788abb69386228df8
Ivan Mironchik <me.roaiven@gmail.com>
2016-05-14 17:45:18 +0300

    Implemented List model

---
+++ server/_models/List.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const shortid = require('shortid');
+
+module.exports = function (sequelize, DataTypes) {
+    const List = sequelize.define('List', {
+        id: {
+            type: DataTypes.STRING,
+            defaultValue: shortid.generate,
+            primaryKey: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            validate: {
+                min: {
+                    args: 1,
+                    msg: 'List title must not be empty'
+                }
+            }
+        }
+    }, {
+        classMethods: {
+            associate(models) {
+                List.hasMany(models.Card);
+            }
+        }
+    });
+
+    return List;
+};
