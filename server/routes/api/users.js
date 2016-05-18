96cf0812501c6201eeafe64ba27278001d8c17c6
Ivan Mironchik <me.roaiven@gmail.com>
2016-05-18 15:15:34 +0300

    Removed `findById` user route

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -2,6 +2,5 @@ const router = require('express').Router();
 const UserController = require('../../controllers/UserController');
 
 router.post('/', UserController.create);
-router.get('/:id', UserController.findById);
 
 module.exports = router;
