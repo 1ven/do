453290eaa7df5d6e4816bc62a555924ce79eea55
Ivan Mironchik <me.roaiven@gmail.com>
2016-05-19 12:53:37 +0300

    Added setup helper

--- test/server/models/Card.spec.js
+++ test/server/models/Card.spec.js
@@ -4,25 +4,18 @@ import { recreateTables } from '../helpers';
 import db from 'server/db';
 import Card from 'server/models/Card';
 
-const listData = {
-    id: shortid.generate(),
-    title: 'test list 1'
-};
-
-const cardData = {
-    id: shortid.generate(),
-    text: 'test card 1'
-};
+const setup = require('../helpers').setup();
+const _card = setup.data.cards[0];
 
 describe('Card', () => {
-    beforeEach(() => recreateTables().then(setup));
+    beforeEach(() => recreateTables().then(setup.create));
 
     describe('update', () => {
         it('should update card and return updated card', () => {
-            return Card.update(cardData.id, { text: 'updated text' })
+            return Card.update(_card.id, { text: 'updated text' })
                 .then(card => {
                     assert.deepEqual(card, {
-                        id: cardData.id,
+                        id: _card.id,
                         text: 'updated text'
                     });
                 });
@@ -31,9 +24,9 @@ describe('Card', () => {
 
     describe('drop', () => {
         it('should drop card entry', () => {
-            return Card.drop(cardData.id)
+            return Card.drop(_card.id)
                 .then(() => {
-                    return db.query(`SELECT id FROM cards WHERE id = $1`, [cardData.id]);
+                    return db.query(`SELECT id FROM cards WHERE id = $1`, [_card.id]);
                 })
                 .then(result => {
                     assert.lengthOf(result, 0);
@@ -41,16 +34,16 @@ describe('Card', () => {
         });
 
         it('should return dropped card id', () => {
-            return Card.drop(cardData.id)
+            return Card.drop(_card.id)
                 .then(result => {
-                    assert.equal(result.id, cardData.id);
+                    assert.equal(result.id, _card.id);
                 });
         });
 
         it('should remove relations', () => {
-            return Card.drop(cardData.id)
+            return Card.drop(_card.id)
                 .then(() => {
-                    return db.query(`SELECT card_id FROM lists_cards WHERE card_id = $1`, [cardData.id]);
+                    return db.query(`SELECT card_id FROM lists_cards WHERE card_id = $1`, [_card.id]);
                 })
                 .then(result => {
                     assert.lengthOf(result, 0);
@@ -58,14 +51,3 @@ describe('Card', () => {
         });
     });
 });
-
-function setup() {
-    return db.none(`
-        INSERT INTO lists (id, title)
-            VALUES ($1, $3);
-        INSERT INTO cards (id, text)
-            VALUES ($2, $4);
-        INSERT INTO lists_cards
-            VALUES ($1, $2);
-    `, [listData.id, cardData.id, listData.title, cardData.text]);
-};
