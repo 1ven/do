1515cae60fb3420f8a99760757582fcfcab1de21
Ivan Mironchik <me.roaiven@gmail.com>
2016-05-19 10:45:24 +0300

    Implemented Card model

--- server/models/Card.js
+++ server/models/Card.js
@@ -1,9 +1,25 @@
-const db = require('db');
+const _ = require('lodash');
+const pgp = require('pg-promise');
+const db = require('../db');
 
 const Card = {
-    update(data) {},
+    update(id, data) {
+        const _data = _.pick(data, ['text']);
 
-    drop(id) {}
+        if (_.isEmpty(_data)) return;
+
+        const props = _.keys(_data).map(k => pgp.as.name(k)).join();
+        const values = _.values(_data);
+
+        return db.one(
+            `UPDATE cards SET ($2^) = ($3:csv) WHERE id = $1 RETURNING id, text`,
+            [id, props, values]
+        );
+    },
+
+    drop(id) {
+        return db.one(`DELETE FROM cards WHERE id = $1 RETURNING id`, [id]);
+    }
 };
 
 module.exports = Card;
