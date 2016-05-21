66086218a8a87f8cd2af8e3213ca6f40e170c2ff
Ivan Mironchik <me.roaiven@gmail.com>
2016-05-21 11:25:18 +0300

    Implemented sanitize util

---
+++ test/server/utils/sanitize.spec.js
@@ -0,0 +1,14 @@
+import { assert } from 'chai';
+import sanitize from 'server/utils/sanitize';
+
+describe('sanitize', () => {
+    it('should sanitize given props', () => {
+        const props = {
+            title: `<script>alert('Hello')</script>`
+        };
+        const sanitized = sanitize(props);
+        assert.deepEqual(sanitized, {
+            title: '&lt;script&gt;alert(&#39;Hello&#39;)&lt;/script&gt;'
+        });
+    });
+});
