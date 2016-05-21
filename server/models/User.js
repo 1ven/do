02cc5e7e910e886389dafb44b941795c129538e7
Ivan Mironchik <me.roaiven@gmail.com>
2016-05-21 09:07:25 +0300

    Implemented id generating in User model tests

--- server/models/User.js
+++ server/models/User.js
@@ -1,29 +1,14 @@
 const _ = require('lodash');
+const shortid = require('shortid');
 const pgp = require('pg-promise');
 const db = require('../db');
 
 const User = {
-    update(id, data) {
-        const _data = _.pick(data, ['username']);
-
-        if (_.isEmpty(_data)) return;
-
-        const props = _.keys(_data).map(k => pgp.as.name(k)).join();
-        const values = _.values(_data);
-
-        return db.one(
-            `UPDATE users SET ($2^) = ($3:csv) WHERE id = $1 RETURNING id, username`,
-            [id, props, values]
-        );
-    },
-
-    drop(id) {
-        return db.one(`DELETE FROM users WHERE id = $1 RETURNING id`, [id]);
-    },
-
     createBoard(userId, boardData) {
+        const id = shortid.generate();
+
         return db.one(`INSERT INTO boards (id, title) VALUES ($1, $2) RETURNING id, title`,
-        [boardData.id, boardData.title])
+        [id, boardData.title])
             .then(board => {
                 return db.none(`INSERT INTO users_boards VALUES ($1, $2)`, [userId, board.id])
                     .then(() => board);
--- test/server/models/User.spec.js
+++ test/server/models/User.spec.js
@@ -4,58 +4,43 @@ import { recreateTables } from '../helpers';
 import db from 'server/db';
 import User from 'server/models/User';
 
-const setup = require('../helpers').setup();
-const _user = setup.data.users[0];
+const userId = shortid.generate();
 
 describe('User', () => {
-    beforeEach(() => recreateTables().then(setup.create));
-
-    describe('update', () => {
-        it('should update user and return updated user', () => {
-            return User.update(_user.id, { username: 'updated' })
-                .then(user => {
-                    assert.deepEqual(user.username, 'updated');
-                });
-        });
-    });
-
-    describe('drop', () => {
-        it('should drop user entry', () => {
-            return User.drop(_user.id)
-                .then(() => {
-                    return db.query(`SELECT id FROM users WHERE id = $1`, [_user.id]);
-                })
-                .then(result => {
-                    assert.lengthOf(result, 0);
-                });
-        });
-
-        it('should return dropped user id', () => {
-            return User.drop(_user.id)
-                .then(result => {
-                    assert.equal(result.id, _user.id);
-                });
-        });
-    });
+    beforeEach(() => recreateTables().then(setup));
 
     describe('createBoard', () => {
         const boardData = {
-            id: shortid.generate(),
             title: 'test board'
         };
 
         it('should create board', () => {
-            return User.createBoard(_user.id, boardData).then(board => {
+            return User.createBoard(userId, boardData).then(board => {
+                assert.property(board, 'id');
+                delete board.id;
                 assert.deepEqual(board, boardData);
             });
         });
 
         it('should relate board to user', () => {
-            return User.createBoard(_user.id, boardData).then(board => {
+            return User.createBoard(userId, boardData).then(board => {
                 return db.one('SELECT user_id FROM users_boards WHERE board_id = $1', [board.id]);
             }).then(result => {
-                assert.equal(result.user_id, _user.id);
+                assert.equal(result.user_id, userId);
+            });
+        });
+
+        it('should generate shortid', () => {
+            return User.createBoard(userId, boardData).then(board => {
+                assert.isTrue(shortid.isValid(board.id));
             });
         });
     });
 });
+
+function setup() {
+    return db.none(`
+        INSERT INTO users (id, username, email, hash, salt)
+        VALUES ($1, 'test', 'test@test.com', 'hash', 'salt');
+    `, [userId]);
+};
