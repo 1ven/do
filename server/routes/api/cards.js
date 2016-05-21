7d83bab643edec75edbf3f26b0623a035656706c
Ivan Mironchik <me.roaiven@gmail.com>
2016-05-21 11:44:26 +0300

    Connected routes

--- server/routes/api/cards.js
+++ server/routes/api/cards.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const CardController = require('../../controllers/CardController');
 
 router.put('/:id', CardController.update);
-router.delete('/:id', CardController.delete);
+router.delete('/:id', CardController.drop);
 
 module.exports = router;
--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 
 const cards = require('./cards');
-const lists = require('./lists');
-const boards = require('./boards');
-const users = require('./users');
+/* const lists = require('./lists'); */
+/* const boards = require('./boards'); */
+/* const users = require('./users'); */
 
 router.use('/cards', cards);
-router.use('/lists', lists);
-router.use('/boards', boards);
-router.use('/users', users);
+/* router.use('/lists', lists); */
+/* router.use('/boards', boards); */
+/* router.use('/users', users); */
 
 module.exports = router;
