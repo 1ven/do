3972eac33a229c80b4d1d2ca82a820161312019b
Ivan Mironchik <me.roaiven@gmail.com>
2016-05-21 12:30:30 +0300

    Connected users routes. Fixed corresponding tests

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -3,11 +3,11 @@ const router = require('express').Router();
 const cards = require('./cards');
 const lists = require('./lists');
 const boards = require('./boards');
-/* const users = require('./users'); */
+const users = require('./users');
 
 router.use('/cards', cards);
 router.use('/lists', lists);
 router.use('/boards', boards);
-/* router.use('/users', users); */
+router.use('/users', users);
 
 module.exports = router;
