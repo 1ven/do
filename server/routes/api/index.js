6bfe76c2dcfbeb3641a653ea232e0eab1a81baf6
Ivan Mironchik <me.roaiven@gmail.com>
2016-05-21 11:54:16 +0300

    Implemented ListController

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 
 const cards = require('./cards');
-/* const lists = require('./lists'); */
+const lists = require('./lists');
 /* const boards = require('./boards'); */
 /* const users = require('./users'); */
 
 router.use('/cards', cards);
-/* router.use('/lists', lists); */
+router.use('/lists', lists);
 /* router.use('/boards', boards); */
 /* router.use('/users', users); */
 
