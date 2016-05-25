a4bc02b0f5151dc69503458a84a81769216ef103
Ivan Mironchik <me.roaiven@gmail.com>
2016-05-25 13:18:09 +0300

    Implemented `createComment` method

--- server/models/Card.js
+++ server/models/Card.js
@@ -1,6 +1,8 @@
 const _ = require('lodash');
+const shortid = require('shortid');
 const pgp = require('pg-promise');
 const db = require('../db');
+const Comment = require('./Comment');
 
 const Card = {
     update(id, data) {
@@ -19,6 +21,18 @@ const Card = {
 
     drop(id) {
         return db.one(`DELETE FROM cards WHERE id = $1 RETURNING id`, [id]);
+    },
+
+    createComment(userId, cardId, commentData) {
+        const commentId = shortid.generate();
+
+        return db.none(`
+            INSERT INTO comments(id, text) VALUES ($3, $4);
+            INSERT INTO cards_comments VALUES ($2, $3);
+            INSERT INTO users_comments VALUES ($1, $3)
+        `, [userId, cardId, commentId, commentData.text])
+            .then(() => Comment.findByIds([commentId]))
+            .then(comments => comments[0]);
     }
 };
 
--- test/server/models/Card.spec.js
+++ test/server/models/Card.spec.js
@@ -1,10 +1,12 @@
 import { assert } from 'chai';
+import _ from 'lodash';
 import shortid from 'shortid';
 import { recreateTables } from '../helpers';
 import db from 'server/db';
 import Card from 'server/models/Card';
 
 const cardId = shortid.generate();
+const userId = shortid.generate();
 
 describe('Card', () => {
     beforeEach(() => recreateTables().then(setup));
@@ -39,10 +41,56 @@ describe('Card', () => {
                 });
         });
     });
+
+    describe('createComment', () => {
+        const commentData = {
+            text: 'test comment'
+        };
+
+        it('should create comment', () => {
+            return Card.createComment(userId, cardId, commentData)
+                .then(comment => {
+                    assert.property(comment, 'id');
+                    assert.property(comment, 'created_at');
+
+                    assert.deepEqual(_.omit(comment, ['id', 'created_at']), {
+                        text: commentData.text,
+                        user: {
+                            id: userId,
+                            username: 'testuser'
+                        }
+                    });
+                });
+        });
+
+        it('should generate shortid', () => {
+            return Card.createComment(userId, cardId, commentData).then(comment => {
+                assert.isTrue(shortid.isValid(comment.id));
+            });
+        });
+
+        it('should relate comment to card', () => {
+            return Card.createComment(userId, cardId, commentData).then(comment => {
+                return db.one(`SELECT card_id FROM cards_comments WHERE comment_id = $1`, [comment.id]);
+            }).then(result => {
+                assert.equal(result.card_id, cardId);
+            });
+        });
+
+        it('should relate comment to user', () => {
+            return Card.createComment(userId, cardId, commentData).then(comment => {
+                return db.one(`SELECT user_id FROM users_comments WHERE comment_id = $1`, [comment.id]);
+            }).then(result => {
+                assert.equal(result.user_id, userId);
+            });
+        });
+    });
 });
 
 function setup() {
     return db.none(`
-        INSERT INTO cards (id, text) VALUES ($1, 'test card 1');
-    `, [cardId]);
+        INSERT INTO users (id, username, email, hash, salt)
+            VALUES ($1, 'testuser', 'testuser@test.com', 'hash', 'salt');
+        INSERT INTO cards (id, text) VALUES ($2, 'test card 1');
+    `, [userId, cardId]);
 };
