588eafed943ad159f58b0396701c821a24a77f09
Ivan Mironchik <me.roaiven@gmail.com>
2016-05-25 08:14:13 +0300

    Implemented <Form />

---
+++ client/components/Form.js
@@ -0,0 +1,43 @@
+import React, { PropTypes, Component } from 'react';
+import ReactDOM from 'react-dom';
+import serialize from 'form-serialize';
+
+class Form extends Component {
+    constructor(props) {
+        super(props);
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        const form = ReactDOM.findDOMNode(this.refs.form);
+        const formData = serialize(form, {
+            hash: true
+        });
+
+        this.props.onSubmit(formData);
+    }
+
+    render() {
+        const { children, className } = this.props;
+        return (
+            <form
+                className={className}
+                onSubmit={this.handleSubmit}
+                ref="form"
+            >
+                {children}
+            </form>
+        );
+    }
+};
+
+Form.propTypes = {
+    children: PropTypes.node.isRequired,
+    onSubmit: PropTypes.func.isRequired,
+    className: PropTypes.string
+};
+
+export default Form;
