9955a743f496c84e2056f0ef996db7c470feceae
Ivan Mironchik <me.roaiven@gmail.com>
2016-05-26 11:07:54 +0300

    Implemented comment schema

---
+++ client/schemas/commentSchema.js
@@ -0,0 +1,5 @@
+import { Schema, arrayOf } from 'normalizr';
+
+const commentSchema = new Schema('comments');
+
+export default commentSchema;
--- client/schemas/index.js
+++ client/schemas/index.js
@@ -3,6 +3,7 @@ import boardSchema from './boardSchema';
 import listSchema from './listSchema';
 import cardSchema from './cardSchema';
 import userSchema from './userSchema';
+import commentSchema from './commentSchema';
 
 export const BOARD = boardSchema;
 export const BOARD_ARRAY = arrayOf(boardSchema);
@@ -11,3 +12,4 @@ export const LIST_ARRAY = arrayOf(listSchema);
 export const CARD = cardSchema;
 export const CARD_ARRAY = arrayOf(cardSchema);
 export const USER = userSchema;
+export const COMMENT = commentSchema;
