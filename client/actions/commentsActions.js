bde81bb741285511538fea2b7ac374e54eda7388
Ivan Mironchik <me.roaiven@gmail.com>
2016-05-26 13:09:03 +0300

    Removed `getComments` action creator

--- client/actions/commentsActions.js
+++ client/actions/commentsActions.js
@@ -21,20 +21,3 @@ export function createComment(cardId, text) {
         }
     };
 };
-
-export function getComments(cardId) {
-    return {
-        [CALL_API]: {
-            types: [
-                types.COMMENTS_GET_REQUEST,
-                types.COMMENTS_GET_SUCCESS,
-                types.COMMENTS_GET_ERROR
-            ],
-            endpoint: `/api/cards/${cardId}/comments`,
-            schema: COMMENT,
-            request: {
-                method: 'get'
-            }
-        }
-    };
-};
