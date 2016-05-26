1528d6ef385dd44d3b1e1e7af4e00da0cab60834
Ivan Mironchik <me.roaiven@gmail.com>
2016-05-26 13:18:19 +0300

    Assigned cardSchema with commentSchema

--- client/schemas/cardSchema.js
+++ client/schemas/cardSchema.js
@@ -1,5 +1,10 @@
 import { Schema, arrayOf } from 'normalizr';
+import commentSchema from './commentSchema';
 
 const cardSchema = new Schema('cards');
+
+cardSchema.define({
+    comments: arrayOf(commentSchema)
+});
 
 export default cardSchema;
