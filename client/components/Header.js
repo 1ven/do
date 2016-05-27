740bf443bc16ff55c63c609d034b2c2304079f40
Ivan Mironchik <me.roaiven@gmail.com>
2016-05-27 19:29:13 +0300

    Fixed card stylesheet. Added <SearchBox /> to <Header />

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,5 +1,32 @@
 import React, { PropTypes } from 'react';
+import SearchBox from './SearchBox';
 import UserBoxContainer from '../containers/UserBoxContainer';
+
+const results = [{
+    type: 'Boards',
+    items: [{
+        title: 'Programming',
+        href: ''
+    }, {
+        title: 'Programmist',
+        href: ''
+    }, {
+        title: 'Programmable',
+        href: ''
+    }]
+}, {
+    type: 'Lists',
+    items: [{
+        title: 'Work',
+        href: ''
+    }, {
+        title: 'Working',
+        href: ''
+    }, {
+        title: 'Worker',
+        href: ''
+    }]
+}];
 
 function Header() {
     return (
@@ -7,6 +34,10 @@ function Header() {
             <div className="b-container">
                 <div className="b-header__wrap">
                     <div className="b-header__left">
+                        <SearchBox
+                            onSubmit={value => console.log(value)}
+                            results={results}
+                        />
                     </div>
                     <div className="b-header__right">
                         <div className="b-header__item">
