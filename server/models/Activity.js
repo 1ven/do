11db47824f400393737c655f65890ded0d183a84
Ivan Mironchik <me.roaiven@gmail.com>
2016-05-30 20:34:59 +0300

    Implemented Activity model

--- server/models/Activity.js
+++ server/models/Activity.js
@@ -1,3 +1,6 @@
+const Promise = require('bluebird');
+const _ = require('lodash');
+const inflect = require('i')();
 const db = require('../db');
 
 const Activity = {
@@ -6,7 +9,32 @@ const Activity = {
             INSERT INTO activity(entry_id, entry_table, action)
             VALUES ($1, $2, $3)
             RETURNING id, created_at, entry_id, entry_table, action
-        `, [entryId, entryTable, action]);
+        `, [entryId, entryTable, action]).then(this._transformActivityItem);
+    },
+
+    findLast() {
+        return db.query(`
+            SELECT id, created_at, entry_id, entry_table, action
+            FROM activity AS a LIMIT 15
+        `).then(activity => {
+            return Promise.map(activity, this._transformActivityItem);
+        });
+    },
+
+    _transformActivityItem(item) {
+        const col = item.entry_table === 'cards' ? 'text' : 'title';
+
+        return db.one(`
+            SELECT id, $3~ AS title, link FROM $2~ WHERE id = $1
+        `, [item.entry_id, item.entry_table, col])
+            .then(entry => {
+                const _item = _.omit(item, ['entry_id', 'entry_table']);
+
+                return _.assign({}, _item, {
+                    type: inflect.singularize(item.entry_table),
+                    entry: _.pick(entry, ['title', 'link'])
+                });
+            });
     }
 };
 
--- test/server/models/Activity.spec.js
+++ test/server/models/Activity.spec.js
@@ -5,28 +5,99 @@ import shortid from 'shortid';
 import { recreateTables } from '../helpers';
 import Activity from 'server/models/Activity';
 
+const boardId = shortid.generate();
+const listId = shortid.generate();
 const cardId = shortid.generate();
 
 describe('Activity', () => {
-    beforeEach(recreateTables);
+    beforeEach(() => recreateTables().then(setup));
 
     describe('create', () => {
         it('should create activity and return created entry', () => {
-            return db.none('INSERT INTO cards(id, text) VALUES ($1, $2)', [cardId, 'test card'])
-                .then(() => Activity.create(cardId, 'cards', 'created'))
+            return Activity.create(cardId, 'cards', 'Created')
                 .then(entry => {
                     assert.property(entry, 'id');
                     assert.property(entry, 'created_at');
                     assert.deepEqual(_.omit(entry, ['id', 'created_at']), {
-                        action: 'created',
-                        table: 'cards',
+                        action: 'Created',
+                        type: 'card',
                         entry: {
-                            id: cardId,
-                            text: 'test card',
+                            title: 'test card',
                             link: `/boards/${boardId}/cards/${cardId}`
                         }
                     });
                 });
         });
     });
+
+    describe('findLast', () => {
+        it('should return last 15 activity entries', () => {
+            return Activity.findLast().then(activity => {
+                assert.lengthOf(activity, 15);
+                assert.property(activity[0], 'created_at');
+                assert.deepEqual(_.omit(activity[0], ['created_at']), {
+                    id: 1,
+                    action: 'Created',
+                    type: 'card',
+                    entry: {
+                        title: 'test card',
+                        link: `/boards/${boardId}/cards/${cardId}`
+                    }
+                });
+            });
+        });
+    });
+
+    describe('_transformActivityItem', () => {
+        it('should append to activity item corresponding entry data and remove entry_id, entry_table', () => {
+            return db.one(`
+                SELECT id, created_at, action, entry_id, entry_table
+                FROM activity WHERE id = 1
+            `).then(activityItem => {
+                return Activity._transformActivityItem(activityItem);
+            }).then(transformed => {
+                assert.property(transformed, 'created_at');
+                assert.deepEqual(_.omit(transformed, ['created_at']), {
+                    id: 1,
+                    action: 'Created',
+                    type: 'card',
+                    entry: {
+                        title: 'test card',
+                        link: `/boards/${boardId}/cards/${cardId}`
+                    }
+                });
+            });
+        });
+    });
 });
+
+function setup() {
+    return db.none(`
+        INSERT INTO boards (id, title) VALUES ($1, 'test board');
+        INSERT INTO lists (id, title) VALUES ($2, 'test list');
+        INSERT INTO boards_lists VALUES ($1, $2);
+        INSERT INTO cards (id, text) VALUES ($3, 'test card');
+        INSERT INTO lists_cards VALUES ($2, $3);
+        INSERT INTO activity (entry_id, entry_table, action) VALUES
+            ($3, 'cards', 'Created'),
+            ($3, 'cards', 'Updated'),
+            ($3, 'cards', 'Updated'),
+            ($3, 'cards', 'Updated'),
+            ($3, 'cards', 'Updated'),
+            ($3, 'cards', 'Updated'),
+            ($3, 'cards', 'Updated'),
+            ($3, 'cards', 'Updated'),
+            ($3, 'cards', 'Updated'),
+            ($3, 'cards', 'Updated'),
+            ($3, 'cards', 'Updated'),
+            ($3, 'cards', 'Updated'),
+            ($3, 'cards', 'Updated'),
+            ($3, 'cards', 'Updated'),
+            ($3, 'cards', 'Updated'),
+            ($3, 'cards', 'Updated'),
+            ($3, 'cards', 'Updated'),
+            ($3, 'cards', 'Updated'),
+            ($3, 'cards', 'Updated'),
+            ($3, 'cards', 'Updated')
+    `, [boardId, listId, cardId]);
+}
