bbc08e200d2581af3aa1120893e1933cad9711d7
Ivan Mironchik <me.roaiven@gmail.com>
2016-06-02 12:08:54 +0300

    Implemented <ProgressBar />

--- client/components/App.js
+++ client/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import NotificationsContainer from '../containers/NotificationsContainer';
 import Header from './Header';
+import ProgressBar from './ProgressBar';
 
 function App({ children }) {
     return (
         <div className="b-app">
+            <ProgressBar progress={50} />
             <Header />
             <div className="b-route-handler">
                 {children}
