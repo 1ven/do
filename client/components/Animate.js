540adf5904586b5758c1c62cd16c7e1ff6f1e10b
Ivan Mironchik <me.roaiven@gmail.com>
2016-06-07 15:11:26 +0300

    Implemented <Animate />

---
+++ client/components/Animate.js
@@ -0,0 +1,21 @@
+import React, { PropTypes } from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
+function Animate({ children, name }) {
+    return (
+        <ReactCSSTransitionGroup
+            transitionName={name}
+            transitionEnterTimeout={150}
+            transitionLeaveTimeout={150}
+        >
+            {children}
+        </ReactCSSTransitionGroup>
+    );
+};
+
+Animate.propTypes = {
+    children: PropTypes.node,
+    name: PropTypes.string.isRequired
+};
+
+export default Animate;
