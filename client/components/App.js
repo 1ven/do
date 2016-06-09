b79ed2808dfd2de6c32ef0d6bfca805769d36761
Ivan Mironchik <me.roaiven@gmail.com>
2016-06-09 12:26:39 +0300

    Removed routes fade in animation

--- client/components/App.js
+++ client/components/App.js
@@ -2,24 +2,14 @@ import React, { PropTypes } from 'react';
 import Header from './Header';
 import NotificationsContainer from '../containers/NotificationsContainer';
 import ProgressBarContainer from '../containers/ProgressBarContainer';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
-function App({ children, location }) {
+function App({ children }) {
   return (
     <div className="b-app">
       <ProgressBarContainer />
       <Header />
       <div className="b-route-handler">
-        <ReactCSSTransitionGroup
-          component="div"
-          transitionName="a-fade-enter"
-          transitionEnterTimeout={300}
-          transitionLeaveTimeout={1}
-        >
-          {React.cloneElement(children, {
-            key: location.pathname,
-          })}
-        </ReactCSSTransitionGroup>
+        {children}
       </div>
       <NotificationsContainer />
     </div>
