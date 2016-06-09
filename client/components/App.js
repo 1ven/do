d1f08613a94bbaac37b3c3f061983c308493f700
Ivan Mironchik <me.roaiven@gmail.com>
2016-06-09 09:45:31 +0300

    Implemented fade in routes animation

--- client/components/App.js
+++ client/components/App.js
@@ -2,14 +2,24 @@ import React, { PropTypes } from 'react';
 import Header from './Header';
 import NotificationsContainer from '../containers/NotificationsContainer';
 import ProgressBarContainer from '../containers/ProgressBarContainer';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
-function App({ children }) {
+function App({ children, location }) {
   return (
     <div className="b-app">
       <ProgressBarContainer />
       <Header />
       <div className="b-route-handler">
-        {children}
+        <ReactCSSTransitionGroup
+          component="div"
+          transitionName="a-fade-enter"
+          transitionEnterTimeout={300}
+          transitionLeaveTimeout={1}
+        >
+          {React.cloneElement(children, {
+            key: location.pathname,
+          })}
+        </ReactCSSTransitionGroup>
       </div>
       <NotificationsContainer />
     </div>
