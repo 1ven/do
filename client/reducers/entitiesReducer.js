29cb0edab4ec1ef5944a00b8f1b629f1dc63c946
Ivan Mironchik <me.roaiven@gmail.com>
2016-06-13 10:21:26 +0300

    Removed handling unused actions

--- client/reducers/entitiesReducer.js
+++ client/reducers/entitiesReducer.js
@@ -22,121 +22,6 @@ function cardsReducer(state = {}, action) {
   }
 }
 
-function boardsReducer(state = {}, action) {
-  const payload = action.payload;
-
-  switch (action.type) {
-    case types.BOARDS_ADD_LIST_ID: {
-      const { boardId, listId } = payload;
-      const lists = state[boardId].lists || [];
-
-      return {
-        ...state,
-        [boardId]: {
-          ...state[boardId],
-          lists: [...lists, listId]
-        },
-      };
-    }
-    case types.BOARDS_REMOVE_LIST_ID: {
-      const { boardId, listId } = payload;
-      const lists = state[boardId].lists || [];
-
-      return {
-        ...state,
-        [boardId]: {
-          ...state[boardId],
-          lists: without(lists, listId)
-        },
-      };
-    }
-    case types.BOARDS_INC_LISTS_LENGTH: {
-      const { boardId } = payload;
-      const board = state[boardId];
-
-      return {
-        ...state,
-        [boardId]: {
-          ...board,
-          listsLength: board.listsLength + 1,
-        },
-      };
-    }
-    case types.BOARDS_DEC_LISTS_LENGTH: {
-      const { boardId } = payload;
-      const board = state[boardId];
-
-      return {
-        ...state,
-        [boardId]: {
-          ...board,
-          listsLength: board.listsLength - 1,
-        },
-      };
-    }
-    case types.BOARDS_INC_CARDS_LENGTH: {
-      const { boardId } = payload;
-      const board = state[boardId];
-
-      return {
-        ...state,
-        [boardId]: {
-          ...board,
-          cardsLength: board.cardsLength + 1,
-        },
-      };
-    }
-    case types.BOARDS_DEC_CARDS_LENGTH: {
-      const { boardId } = payload;
-      const board = state[boardId];
-
-      return {
-        ...state,
-        [boardId]: {
-          ...board,
-          cardsLength: board.cardsLength -1,
-        },
-      };
-    }
-    default:
-      return state;
-  }
-}
-
-function listsReducer(state = {}, action) {
-  const payload = action.payload;
-
-  switch (action.type) {
-    case types.LISTS_ADD_CARD_ID: {
-      // TODO: Remove variables duplicating.
-      const { listId, cardId } = payload;
-      const cards = state[listId].cards || [];
-
-      return {
-        ...state,
-        [listId]: {
-          ...state[listId],
-          cards: [...cards, cardId],
-        },
-      };
-    }
-    case types.LISTS_REMOVE_CARD_ID: {
-      const { listId, cardId } = payload;
-      const cards = state[listId].cards || [];
-
-      return {
-        ...state,
-        [listId]: {
-          ...state[listId],
-          cards: without(cards, cardId),
-        },
-      };
-    }
-    default:
-      return state;
-  }
-}
-
 export default function entities(state = {}, action) {
   const payload = action.payload;
 
@@ -145,8 +30,8 @@ export default function entities(state = {}, action) {
   }
 
   return {
-    boards: boardsReducer(state.boards, action),
-    lists: listsReducer(state.lists, action),
+    boards: state.boards || {},
+    lists: state.lists || {},
     cards: cardsReducer(state.cards, action),
     users: state.users || {},
     comments: state.comments || {},
