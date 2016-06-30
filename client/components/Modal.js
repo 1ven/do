cb5099d2983b6bf5ca52fa27e71154da6e2ee690
Ivan Mironchik <me.roaiven@gmail.com>
2016-06-30 13:48:22 +0300

    Displaying spinner

--- client/components/Modal.js
+++ client/components/Modal.js
@@ -1,27 +1,41 @@
 import React, { PropTypes } from 'react';
+import RoundSpinner from './RoundSpinner';
 import Icon from './Icon';
 
-function Modal({ title, children, onCloseClick }) {
+function Modal({
+  title,
+  children,
+  isWaiting,
+  onCloseClick,
+}) {
   return (
     <div className="b-modal">
-      <div className="b-modal__box">
-        <div className="b-modal-box">
-          <div className="b-modal-box__top">
-            <span className="b-modal-box__title">
-              {title}
-            </span>
-            <a
-              className="b-modal-box__close"
-              onClick={onCloseClick}
-            >
-              <Icon name="cross" />
-            </a>
-          </div>
-          <div className="b-modal-box__content">
-            {children}
+      {isWaiting ? (
+        <RoundSpinner
+          size="30px"
+          color="#fff"
+          thickness="3px"
+        />
+      ) : (
+        <div className="b-modal__box">
+          <div className="b-modal-box">
+            <div className="b-modal-box__top">
+              <span className="b-modal-box__title">
+                {title}
+              </span>
+              <a
+                className="b-modal-box__close"
+                onClick={onCloseClick}
+              >
+                <Icon name="cross" />
+              </a>
+            </div>
+            <div className="b-modal-box__content">
+              {children}
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
