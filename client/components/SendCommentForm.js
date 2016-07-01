b5a6338479049eb29d9bc3fd3ad05ec076baa5db
Ivan Mironchik <me.roaiven@gmail.com>
2016-07-01 15:02:04 +0300

    Clearing form after submit

--- client/components/SendCommentForm.js
+++ client/components/SendCommentForm.js
@@ -1,28 +1,56 @@
-import React, { PropTypes } from 'react';
-import Input from './Input';
+import React, { PropTypes, Component } from 'react';
 import Btn from './Btn';
 import Form from './Form';
 
-function SendCommentForm({ onSubmit }) {
-  return (
-    <Form
-      className="b-send-comment-form"
-      onSubmit={onSubmit}
-    >
-      <Input
-        name="text"
-        placeholder="Enter your text"
-      />
-      <div className="b-send-comment-form__submit">
-        <Btn
-          modifiers={['sm', 'full-width']}
-          text="Send"
-          type="Submit"
-          tagName="button"
+class SendCommentForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: '',
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleSubmit(formData) {
+    this.setState({
+      value: '',
+    });
+    this.props.onSubmit(formData);
+  }
+
+  handleInputChange(e) {
+    this.setState({
+      value: e.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <Form
+        className="b-send-comment-form"
+        onSubmit={this.handleSubmit}
+      >
+        <input
+          className="b-input"
+          value={this.state.value}
+          onChange={this.handleInputChange}
+          name="text"
+          placeholder="Enter your text"
         />
-      </div>
-    </Form>
-  );
+        <div className="b-send-comment-form__submit">
+          <Btn
+            modifiers={['sm', 'full-width']}
+            text="Send"
+            type="Submit"
+            tagName="button"
+          />
+        </div>
+      </Form>
+    );
+  }
 }
 
 SendCommentForm.propTypes = {
