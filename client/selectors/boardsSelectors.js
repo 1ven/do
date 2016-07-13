cdc8d1a2dcc5da51c6c6ecabd4a8dfe5e209404e
Ivan Mironchik <me.roaiven@gmail.com>
2016-07-13 15:41:55 +0300

    Add `getBoard` selector. Add `error` prop. Remove `makeGetBoards`

--- client/selectors/boardsSelectors.js
+++ client/selectors/boardsSelectors.js
@@ -2,29 +2,30 @@ import { createSelector } from 'reselect';
 import cookie from 'js-cookie';
 import { BOARDS_PER_PAGE } from '../constants/config';
 
-const getBoardsEntities = (state) => state.entities.boards;
-const getIds = (state, { ids }) => ids;
 const getStarredIds = (state) => state.pages.main.starred.ids;
+const getStarredError = (state) => state.pages.main.starred.error;
 const getAllIds = (state) => state.pages.main.all.ids;
 const getAllPageIndex = (state) => state.pages.main.all.pageIndex;
 const getAllCount = (state) => state.pages.main.all.count;
 const getAllSpinner = (state) => state.pages.main.all.isFetching && !!state.pages.main.all.lastUpdated;
+const getAllError = (state) => state.pages.main.all.error;
 
 const getStarredGroup = createSelector(
-  [ getStarredIds ],
-  (ids) => {
+  [ getStarredIds, getStarredError ],
+  (ids, error) => {
     return {
       hidden: !!cookie.get('starred_accordion_hidden'),
       title: 'Starred boards',
       type: 'starred',
       ids,
+      error,
     };
   }
 );
 
 const getAllGroup = createSelector(
-  [ getAllIds, getAllPageIndex, getAllCount, getAllSpinner ],
-  (ids, pageIndex, count, spinner) => {
+  [ getAllIds, getAllPageIndex, getAllCount, getAllSpinner, getAllError ],
+  (ids, pageIndex, count, spinner, error) => {
     return {
       hidden: !!cookie.get('all_accordion_hidden'),
       title: 'My boards',
@@ -32,16 +33,14 @@ const getAllGroup = createSelector(
       ids: ids.filter((id, i) => i < pageIndex * BOARDS_PER_PAGE),
       count,
       spinner, 
+      error,
     };
   }
 );
 
-export const makeGetVisibleBoards = () => createSelector(
-  [ getBoardsEntities, getIds ],
-  (entities, ids) => {
-    return ids.map(id => entities[id]);
-  }
-);
+export const getBoard = (state, { id }) => {
+  return state.entities.boards[id];
+};
 
 export const getGroups = createSelector(
   [ getStarredGroup, getAllGroup ],
