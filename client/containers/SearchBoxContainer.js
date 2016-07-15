c018f937e11c8b7e1d10c13adc934d12d4836190
Ivan Mironchik <me.roaiven@gmail.com>
2016-07-15 13:27:01 +0300

    Returning props using selector

--- client/containers/SearchBoxContainer.js
+++ client/containers/SearchBoxContainer.js
@@ -1,38 +1,11 @@
 import { connect } from 'react-redux';
 import SearchBox from '../components/SearchBox';
 import { search, resetSearch } from '../actions/searchActions';
-
-function prettyResults(results) {
-  return results.reduce((acc, result) => {
-    const isTypeInAcc = acc.filter(group => group.type === result.type).length === 1;
-    const resultItem = {
-      title: result.content,
-      link: result.link,
-    };
-
-    if (!isTypeInAcc) {
-      return [...acc, {
-        type: result.type,
-        items: [resultItem],
-      }];
-    }
-
-    return acc.map(group => {
-      if (group.type === result.type) {
-        return {
-          ...group,
-          items: [...group.items, resultItem],
-        };
-      }
-      return group;
-    });
-  }, []);
-}
+import { getResults } from '../selectors/searchSelectors';
 
 function mapStateToProps(state) {
-  const { results } = state.search;
   return {
-    results: results ? prettyResults(results) : results,
+    results: getResults(state),
     isWaiting: state.search.isFetching,
     query: state.search.query,
   };
