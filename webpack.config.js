34bfdc0488b6fe541857d4f46b195c808264bc06
Ivan Mironchik <me.roaiven@gmail.com>
2016-07-15 11:45:01 +0300

    Add webpack sourcemaps

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
+  devtool: 'eval-source-map',
   entry: [
     'webpack-hot-middleware/client?reload=true',
     path.join(__dirname, 'client/index.js'),
