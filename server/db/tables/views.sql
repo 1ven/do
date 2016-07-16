a2d3fda394543b63f2c6592a83cd6f98629151fc
Ivan Mironchik <me.roaiven@gmail.com>
2016-07-16 16:49:38 +0300

    Not searching by deleted entries

--- server/db/tables/views.sql
+++ server/db/tables/views.sql
@@ -1,10 +1,12 @@
 CREATE OR REPLACE VIEW search AS (
   SELECT id, title AS content, 'Boards' AS type, '/boards/' || id AS link
   FROM boards
+  WHERE deleted is NULL
 ) UNION (
   SELECT c.id, c.text AS content, 'Cards' AS type,
     '/boards/' || bl.board_id || '/cards/' || c.id AS link
   FROM cards AS c
   LEFT JOIN lists_cards AS lc ON (lc.card_id = c.id)
   LEFT JOIN boards_lists AS bl ON (bl.list_id = lc.list_id)
+  WHERE deleted is NULL
 );
