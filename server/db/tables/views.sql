cd548e4ec670f81719c511e316d555ef6faa860c
Ivan Mironchik <me.roaiven@gmail.com>
2016-07-16 15:54:53 +0300

    Remove search limit

--- server/db/tables/views.sql
+++ server/db/tables/views.sql
@@ -1,11 +1,10 @@
 CREATE OR REPLACE VIEW search AS (
   SELECT id, title AS content, 'Boards' AS type, '/boards/' || id AS link
-  FROM boards LIMIT 5
+  FROM boards
 ) UNION (
   SELECT c.id, c.text AS content, 'Cards' AS type,
     '/boards/' || bl.board_id || '/cards/' || c.id AS link
   FROM cards AS c
   LEFT JOIN lists_cards AS lc ON (lc.card_id = c.id)
   LEFT JOIN boards_lists AS bl ON (bl.list_id = lc.list_id)
-  LIMIT 5
 );
