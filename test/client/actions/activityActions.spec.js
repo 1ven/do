043e621cab03339e05019ab644eb9cefc9285e99
Ivan Mironchik <me.roaiven@gmail.com>
2016-07-17 18:03:55 +0300

    Add fetchActivity actions tests

---
+++ test/client/actions/activityActions.spec.js
@@ -0,0 +1,13 @@
+import { assert } from 'chai';
+import types from 'client/constants/actionTypes';
+import { fetchActivity } from 'client/actions/activityActions';
+
+describe('activity actions', () => {
+  describe('fetchActivity', () => {
+    it('should create `fetchActivity` actions', () => {
+      assert.equal(fetchActivity.request().type, types.ACTIVITY_FETCH_REQUEST);
+      assert.equal(fetchActivity.success().type, types.ACTIVITY_FETCH_SUCCESS);
+      assert.equal(fetchActivity.failure().type, types.ACTIVITY_FETCH_FAILURE);
+    });
+  });
+});
