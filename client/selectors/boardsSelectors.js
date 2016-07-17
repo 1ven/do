5f4c9321b3428fb7fefb7dc54399f11fe6a70d0e
Ivan Mironchik <me.roaiven@gmail.com>
2016-07-17 14:14:08 +0300

    Add visibility selectors

--- client/selectors/boardsSelectors.js
+++ client/selectors/boardsSelectors.js
@@ -6,12 +6,14 @@ import { BOARDS_PER_PAGE } from '../constants/config';
 
 const getStarredIds = (state) => state.pages.main.starred.ids;
 const getStarredError = (state) => state.pages.main.starred.error;
+const getStarredVisibility = () => !!cookie.get('starred_accordion_hidden');
 const getAllIds = (state) => state.pages.main.all.ids;
 const getAllPageIndex = (state) => state.pages.main.all.pageIndex;
 const getAllCount = (state) => state.pages.main.all.count;
 const getAllSpinner = (state) => state.pages.main.all.isFetching && !!state.pages.main.all.lastUpdated;
 const getAllError = (state) => state.pages.main.all.error;
 const getBoardEntity = (state, { id }) => state.entities.boards[id];
+const getAllVisibility = () => !!cookie.get('all_accordion_hidden');
 
 const createDeepEqualSelector = createSelectorCreator(
   defaultMemoize,
@@ -19,28 +21,28 @@ const createDeepEqualSelector = createSelectorCreator(
 );
 
 const getStarredGroup = createSelector(
-  [ getStarredIds, getStarredError ],
-  (ids, error) => {
+  [ getStarredIds, getStarredError, getStarredVisibility ],
+  (ids, error, hidden) => {
     return {
-      hidden: !!cookie.get('starred_accordion_hidden'),
       title: 'Starred boards',
       type: 'starred',
       ids,
+      hidden,
       error,
     };
   }
 );
 
 const getAllGroup = createSelector(
-  [ getAllIds, getAllPageIndex, getAllCount, getAllSpinner, getAllError ],
-  (ids, pageIndex, count, spinner, error) => {
+  [ getAllIds, getAllPageIndex, getAllCount, getAllSpinner, getAllError, getAllVisibility ],
+  (ids, pageIndex, count, spinner, error, hidden) => {
     return {
-      hidden: !!cookie.get('all_accordion_hidden'),
       title: 'My boards',
       type: 'all',
       ids: ids.filter((id, i) => i < pageIndex * BOARDS_PER_PAGE),
       count,
       spinner, 
+      hidden,
       error,
     };
   }
