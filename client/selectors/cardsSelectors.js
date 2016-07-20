7526cee568b065f51ec7086f1c787399488fecd5
Ivan Mironchik <me.roaiven@gmail.com>
2016-07-21 00:43:19 +0300

    Add `getCardsLength` selector

--- client/selectors/cardsSelectors.js
+++ client/selectors/cardsSelectors.js
@@ -1,5 +1,6 @@
 import { createSelector } from 'reselect';
 
+export const getCardsLength = (state, { listId }) => state.entities.lists[listId].cards.length;
 const getCardEntity = (state, { id }) => state.entities.cards[id];
 
 const makeGetCardColors = () => createSelector(
