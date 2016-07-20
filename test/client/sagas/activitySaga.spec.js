47efd987b5c228ddced191b61e15909e34c9f13b
Ivan Mironchik <me.roaiven@gmail.com>
2016-07-20 10:31:15 +0300

    Add activitySaga test

---
+++ test/client/sagas/activitySaga.spec.js
@@ -0,0 +1,61 @@
+import { assert } from 'chai';
+import { takeEvery } from 'redux-saga';
+import { call, put } from 'redux-saga/effects';
+import api from 'client/services/api';
+import types from 'client/constants/actionTypes';
+import { fetchActivityTask, watchFetchActivity } from 'client/sagas/activitySaga';
+
+describe('activitySaga', () => {
+  describe('fetchActivityTask', () => {
+    it('should not fail', () => {
+      const payload = {};
+      const gen = fetchActivityTask();
+
+      assert.deepEqual(
+        gen.next().value,
+        call(api.fetchActivity)
+      );
+
+      assert.deepEqual(
+        gen.next(payload).value,
+        put({
+          type: types.ACTIVITY_FETCH_SUCCESS,
+          payload,
+        })
+      );
+
+      assert.isTrue(gen.next().done);
+    });
+
+    it('should fail', () => {
+      const error = new Error('test');
+      const gen = fetchActivityTask();
+
+      assert.deepEqual(
+        gen.next().value,
+        call(api.fetchActivity)
+      );
+
+      assert.deepEqual(
+        gen.throw(error).value,
+        put({
+          type: types.ACTIVITY_FETCH_FAILURE,
+          error: error.message,
+        })
+      );
+
+      assert.isTrue(gen.next().done);
+    });
+  });
+
+  describe('watchFetchActivity', () => {
+    it('should watch every ACTIVITY_FETCH_REQUEST action', () => {
+      const gen = watchFetchActivity();
+
+      assert.deepEqual(
+        gen.next().value,
+        takeEvery(types.ACTIVITY_FETCH_REQUEST, fetchActivityTask).next().value
+      );
+    });
+  });
+});
