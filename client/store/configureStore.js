7fac4fcb8624ec1b65549794fe88cc02697c2c81
Ivan Mironchik <me.roaiven@gmail.com>
2016-07-24 12:49:40 +0300

    Remove `test` middleware

--- client/store/configureStore.js
+++ client/store/configureStore.js
@@ -5,16 +5,6 @@ import rootSaga from '../sagas';
 import rootReducer from '../reducers';
 import DevTools from '../containers/DevTools';
 
-const test = store => next => action => {
-  const prevState = store.getState();
-  const result = next(action);
-  const nextState = store.getState();
-
-  console.log('ent', prevState.entities.boards === nextState.entities.boards);
-  console.log('ids', prevState.pages.main.all.ids === nextState.pages.main.all.ids);
-  return result;
-}
-
 export default function configureStore(initialState) {
   const sagaMiddleware = createSagaMiddleware();
   const logger = createLogger();
